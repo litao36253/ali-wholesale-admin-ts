bf5530fbd1e70cf2ce36c4aad2ccc7231633c8bf
litao <897262216@qq.com>
2021-02-19 18:20:08 +0800

    Update mutations.js

--- src/store/common/mutations.js
+++ src/store/common/mutations.js
@@ -1,5 +1,14 @@
 export default {
   changeUserInfo (state, payload) {
     state.userInfo = payload
+  },
+  changeKeepAliveRoutes (state, payload) {
+    const keepAliveRoutes = new Set(state.keepAliveRoutes)
+    if (payload.type === 'remove') {
+      keepAliveRoutes.remove(payload.name)
+    } else {
+      keepAliveRoutes.add(payload.name)
+    }
+    state.keepAliveRoutes = Array.from(keepAliveRoutes)
   }
 }
