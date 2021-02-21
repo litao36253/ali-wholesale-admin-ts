edcadf8bc606eb292c271b1f785ad60f6294d993
litao <897262216@qq.com>
2021-02-22 00:14:22 +0800

    基础框架搭建

---
+++ src/my.d.ts
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import Vue from 'vue'
+import jql from './server/jql/jql'
+import * as utils from './utils/utils'
+
+declare module 'vue/types/vue' {
+  // eslint-disable-next-line no-unused-vars
+  interface Vue {
+    readonly $jql: typeof jql
+    readonly $utils: typeof utils
+  }
+}
--- src/sfc.d.ts
+++ src/sfc.d.ts
@@ -1,15 +1,4 @@
-import Vue from 'vue'
-import jql from './server/jql/jql'
-import * as utils from './utils/utils'
-
 declare module '*.vue' {
+  import Vue from 'vue'
   export default Vue
-}
-
-declare module 'vue/types/vue' {
-  // eslint-disable-next-line no-unused-vars
-  interface Vue {
-    readonly $jql: typeof jql
-    readonly $utils: typeof utils
-  }
 }
