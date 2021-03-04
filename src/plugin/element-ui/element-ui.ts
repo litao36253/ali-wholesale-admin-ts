5ec7742e309018debc342d5456b8cb0a94f4d85f
litao <897262216@qq.com>
2021-03-04 19:05:39 +0800

    Update element-ui.ts

--- src/plugin/element-ui/element-ui.ts
+++ src/plugin/element-ui/element-ui.ts
@@ -4,6 +4,7 @@ import 'element-ui/lib/theme-chalk/index.css'
 import ElRadioGroup from './el-radio-group.vue'
 import ElCheckboxGroup from './el-checkbox-group.vue'
 import ElSelect from './el-select.vue'
+import ElTree from './el-tree.vue'
 
 // Vue.use(ElementUI, { size: 'small' })
 Vue.use(ElementUI)
@@ -11,3 +12,4 @@ Vue.use(ElementUI)
 Vue.component('el-radio-group', ElRadioGroup)
 Vue.component('el-checkbox-group', ElCheckboxGroup)
 Vue.component('el-select', ElSelect)
+Vue.component('el-tree', ElTree)
